aa4b4d578bb75d3800311202b05b6ee7ea2de9e8
Brett Bohnenkamper <kittykatt@archlinux.us>
2010-03-22 10:32:41 -0500

    Added per-distro ASCII files

---
+++ ascii/logotest.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+
+c0="\e[0m" # End Sequence
+c1="\e[1;32m" # Bold Green
+c2="\e[1;37m" # Bold White
+
+echo -e "$c1 MMMMMMMMMMMMMMMMMMMMMMMMMmds+."
+echo -e "$c1 MMm----::-://////////////oymNMd+\`"
+echo -e "$c1 MMd      "$c2"/++                "$c1"-sNMd:"
+echo -e "$c1 MMNso/\`  "$c2"dMM    \`.::-. .-::.\` "$c1".hMN:"
+echo -e "$c1 ddddMMh  "$c2"dMM   :hNMNMNhNMNMNh: "$c1"\`NMm"
+echo -e "$c1     NMm  "$c2"dMM  .NMN/-+MMM+-/NMN\` "$c1"dMM"
+echo -e "$c1     NMm  "$c2"dMM  -MMm  \`MMM   dMM. "$c1"dMM"
+echo -e "$c1     NMm  "$c2"dMM  -MMm  \`MMM   dMM. "$c1"dMM"
+echo -e "$c1     NMm  "$c2"dMM  .mmd  \`mmm   yMM. "$c1"dMM"
+echo -e "$c1     NMm  "$c2"dMM\`  ..\`   ...   ydm. "$c1"dMM"
+echo -e "$c1     hMM- "$c2"+MMd/-------...-:sdds  "$c1"dMM"
+echo -e "$c1     -NMm- "$c2":hNMNNNmdddddddddy/\`  "$c1"dMM"
+echo -e "$c1      -dMNs-"$c2"\`\`-::::-------.\`\`    "$c1"dMM"
+echo -e "$c1       \`/dMNmy+/:-------------:/yMMM"
+echo -e "$c1          ./ydNMMMMMMMMMMMMMMMMMMMMM"$c0
+echo " "
+echo " "
+
+c3="\e[0;37m" # Light Gray
+
+echo -e "$c3               ___       ___       _"
+echo -e "$c3              /  /      /  /      | |"
+echo -e "$c3             /  /      /  /       | |"
+echo -e "$c3     _______/  /______/  /______  | |"
+echo -e "$c3    /______   _______   _______/  | |"
+echo -e "$c3          /  /      /  /          | |"
+echo -e "$c3         /  /      /  /           | |"
+echo -e "$c3  ______/  /______/  /______      | |"
+echo -e "$c3 /_____   _______   _______/      | |"
+echo -e "$c3      /  /      /  /              |_|"
+echo -e "$c3     /  /      /  /                _"
+echo -e "$c3    /__/      /__/                |_|"$c0
+echo " "
+echo " "
+
+c4="\e[1;31m" # Light Red
+c5="\e[1;33m" # Bold Yellow
+c6="\e[1;37m" # Bold White
+
+echo -e "$c4                          ./+o+-"
+echo -e "$c6                  yyyyy- $c4-yyyyyy+"
+echo -e "$c6               $c6://+//////$c4-yyyyyyo"
+echo -e "$c5           .:: $c6.:/++++++/-$c4.+sss/\`"
+echo -e "$c5         .:----  $c6/++++++++/:--:/-"
+echo -e "$c5        -:---:::.$c6\`..\`\`\`.-/oo+++++/"
+echo -e "$c5       .:--:::/.$c6          \`+sssoo+/"
+echo -e "$c6  .:///:$c5-----:\`$c6             /sssooo."
+echo -e "$c6 -//////:$c5\`----$c6               /::--:."
+echo -e "$c6 -///::::$c5\`----$c4               ++////."
+echo -e "$c6  .:::::$c5-----:\`$c4             /dddhhh."
+echo -e "$c5       .-.----:.$c4          \`oddhhhh+"
+echo -e "$c5        -:.-----\`\`-\`\`$c4\`\`.:ohdhhhhh+"
+echo -e "$c5         \`:---- $c4\`ohhhhhhhhyo++os:"
+echo -e "$c5           .-:$c4\`.syhhhhhhh/$c5.--::-\`"
+echo -e "$c4               /osyyyyyyo$c5-------/"
+echo -e "$c4                   \`\`\`\`\` $c5-:-....:"
+echo -e "$c5                          \`----."$c0
+echo " "
+echo " "
+
+c7="\e[1;31m" # Light Red
+
+echo -e "$c7              ,        ,"
+echo -e "$c7             /(        )\`"
+echo -e "$c7             \ \___   / |"
+echo -e "$c7             /- _  \`-/  '"
+echo -e "$c7            (/\/ \ \   /\\"
+echo -e "$c7            / /   | \`    \\"
+echo -e "$c7            O O   ) /    |"
+echo -e "$c7            \`-^--'\`<     '"
+echo -e "$c7           (_.)  _  )   /"
+echo -e "$c7            \`.___/\`    /"
+echo -e "$c7              \`-----' /"
+echo -e "$c7 <----.     __ / __   \\"
+echo -e "$c7 <----|====O)))==) \) \/===="
+echo -e "$c7 <----'    \`--' \`.__,' \\"
+echo -e "$c7              |        |"
+echo -e "$c7               \       /       /\\"
+echo -e "$c7          ______( (_  / \______/"
+echo -e "$c7        ,'  ,-----'   |"
+echo -e "$c7        \`--{__________)"$c0
+echo " "
+echo " "
+
+echo -e "$c7       _,met\$\$\$\$\$gg."
+echo -e "$c7    ,g\$\$\$\$\$\$\$\$\$\$\$\$\$\$\$P."
+echo -e "$c7  ,g\$\$P\"\"       \"\"\"Y\$\$.\"."
+echo -e "$c7 ,\$\$P'              \`\$\$\$."
+echo -e "$c7',\$\$P       ,ggs.     \`\$\$b:"
+echo -e "$c7\`d\$\$'     ,\$P\"\'   $c6.$c7    \$\$\$"
+echo -e "$c7 \$\$P      d\$\'     $c6,$c7    \$\$P"
+echo -e "$c7 \$\$:      \$\$.   $c6-$c7    ,d\$\$'"
+echo -e "$c7 \$\$\;      Y\$b._   _,d\$P'"
+echo -e "$c7 Y\$\$.    $c6\`.$c7\`\"Y\$\$\$\$P\"'"
+echo -e "$c7 \`\$\$b      $c6\"-.__"
+echo -e "$c7  \`Y\$\$"
+echo -e "$c7   \`Y\$\$."
+echo -e "$c7     \`\$\$b."
+echo -e "$c7       \`Y\$\$b."
+echo -e "$c7          \`\"Y\$b._"
+echo -e "$c7              \`\"\"\"\""$c0
+echo " "
+echo " "
+
+c8="\e[1;39m" # White
+c9="\e[1;34m" # Light Blue
+
+echo -e "$c8          _=(SDGJT=_"
+echo -e "$c8        _GTDJHGGFCVS)"
+echo -e "$c8       ,GTDJGGDTDFBGX0"
+echo -e "$c8      JDJDIJHRORVFSBSVL$c9-=+=,_"
+echo -e "$c8     IJFDUFHJNXIXCDXDSV,$c9  \"DEBL"
+echo -e "$c8    [LKDSDJTDU=OUSCSBFLD.$c9   '?ZWX,"
+echo -e "$c8   ,LMDSDSWH'     \`DCBOSI$c9     DRDS],"
+echo -e "$c8   SDDFDFH'         !YEWD,$c9   )HDROD"
+echo -e "$c8  !KMDOCG            &GSU|$c9\_GFHRGO\'"
+echo -e "$c8  HKLSGP'$c9           __$c8\TKM0$c9\GHRBV)'"
+echo -e "$c8 JSNRVW'$c9       __+MNAEC$c8\IOI,$c9\BN'"
+echo -e "$c8 HELK['$c9    __,=OFFXCBGHC$c8\FD)"
+echo -e "$c8 ?KGHE $c9\_-#DASDFLSV='$c8    'EF"
+echo -e "$c8 'EHTI                   !H"
+echo -e "$c8  \`0F'                   '!"
+
+c10="\e[1;35m" # Light Purple
+
+echo -e "$c10          -/oyddmdhs+:."
+echo -e "$c10      -o"$c8"dNMMMMMMMMNNmhy+"$c10"-\`"
+echo -e "$c10    -y"$c8"NMMMMMMMMMMMNNNmmdhy"$c10"+-"
+echo -e "$c10  \`o"$c8"mMMMMMMMMMMMMNmdmmmmddhhy"$c10"/\`"
+echo -e "$c10  om"$c8"MMMMMMMMMMMN"$c10"hhyyyo"$c8"hmdddhhhd"$c10"o\`"
+echo -e "$c10 .y"$c8"dMMMMMMMMMMd"$c10"hs++so/s"$c8"mdddhhhhdm"$c10"+\`"
+echo -e "$c10  oy"$c8"hdmNMMMMMMMN"$c10"dyooy"$c8"dmddddhhhhyhN"$c10"d."
+echo -e "$c10   :o"$c8"yhhdNNMMMMMMMNNNmmdddhhhhhyym"$c10"Mh"
+echo -e "$c10     .:"$c8"+sydNMMMMMNNNmmmdddhhhhhhmM"$c10"my"
+echo -e "$c10        /m"$c8"MMMMMMNNNmmmdddhhhhhmMNh"$c10"s:"
+echo -e "$c10     \`o"$c8"NMMMMMMMNNNmmmddddhhdmMNhs"$c10"+\`"
+echo -e "$c10   \`s"$c8"NMMMMMMMMNNNmmmdddddmNMmhs"$c10"/."
+echo -e "$c10  /N"$c8"MMMMMMMMNNNNmmmdddmNMNdso"$c10":\`"
+echo -e "$c10 +M"$c8"MMMMMMNNNNNmmmmdmNMNdso"$c10"/-"
+echo -e "$c10 yM"$c8"MNNNNNNNmmmmmNNMmhs+/"$c10"-\`"
+echo -e "$c10 /h"$c8"MMNNNNNNNNMNdhs++/"$c10"-\`"
+echo -e "$c10 \`/"$c8"ohdmmddhys+++/:"$c10".\`"
+echo -e "$c10   \`-//////:--."$c0
+
+c11="\e[1;34m" # Light Blue
+c12="\e[1;39m" # White
+
+echo -e "$c11           :/------------://"
+echo -e "$c11        :------------------://"
+echo -e "$c11      :-----------"$c12"/shhdhyo/"$c11"-://"
+echo -e "$c11    /-----------"$c12"omMMMNNNMMMd/"$c11"-:/"
+echo -e "$c11   :-----------"$c12"sMMMdo:/"$c11"       -:/"
+echo -e "$c11  :-----------"$c12":MMMd"$c11"-------    --:/"
+echo -e "$c11  /-----------"$c12":MMMy"$c11"-------    ---/"
+echo -e "$c11 :------    --"$c12"/+MMMh/"$c11"--        ---:"
+echo -e "$c11 :---     "$c12"oNMMMMMMMMMNho"$c11"     -----:"
+echo -e "$c11 :--      "$c12"+shhhMMMmhhy++"$c11"   ------:"
+echo -e "$c11 :-      -----"$c12":MMMy"$c11"--------------/"
+echo -e "$c11 :-     ------"$c12"/MMMy"$c11"-------------:"
+echo -e "$c11 :-      ----"$c12"/hMMM+"$c11"------------:"
+echo -e "$c11 :--"$c12":dMMNdhhdNMMNo"$c11"-----------:"
+echo -e "$c11 :---"$c12":sdNMMMMNds:"$c11"----------:"
+echo -e "$c11 :------"$c12":://:"$c11"-----------://"
+echo -e "$c11 :--------------------://"$c0
